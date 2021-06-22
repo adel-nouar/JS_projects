2c9a52dfed2b44ee2e769cf0df93249328ab0fcb
Adel NOUAR <adel.nouar@protonmail.com>
2021-06-22 22:49:49 +0200

    Adding Employee and Student subclasses

--- oop/person.js
+++ oop/person.js
@@ -18,14 +18,33 @@ class Person {
     }
 }
 
+class Employee extends Person {
+    constructor(firstName, lastName, age, position, likes ){
+        super(firstName, lastName, age, likes );
+        this.position = position;
+    }
+    getBio() {
+        return `${this.firstName} ${this.lastName} is a ${this.position}`;
+    }
+    getYearsLeft() {
+        return 65 - this.age;
+    }
+}
 
+class Student extends Person {
+    constructor(firstName, lastName, age, grade, likes) {
+        super(firstName, lastName, age, likes);
+        this.grade = grade;
+    }
+    updateGrade(change) {
+        this.grade += change;
+    }
+    getBio() {
+        const status = this.grade >= 70 ? 'passing' : 'failing';
+        return `${this.firstName} is ${status} the class.`
+    }
+}
 
-const me = new Person('Andrew', 'Mead', 27, ['Teaching', 'Biking']);
-
-me.setName('Alexis Turner')
-console.log(me.getBio())
-
-const person2 = new Person('Clancey', 'Turner', 51, []);
-
-
-console.log(person2.getBio())
+const me = new Student('first', 'last', 27, 88, []);
+me.updateGrade(20);
+console.log(me.getBio());
