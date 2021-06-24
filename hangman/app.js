45dbb9071833363486eee6c918089603d948222d
Adel NOUAR <adel.nouar@protonmail.com>
2021-06-24 22:24:20 +0200

    Adding simple HTTP request.

--- hangman/app.js
+++ hangman/app.js
@@ -12,3 +12,14 @@ window.addEventListener('keydown', function (e){
     puzzleEl.textContent = game1.puzzle;
     guessesEl.textContent = game1.statusMessage;
 })
+
+// Making and HTTP request
+const request = new XMLHttpRequest();
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4){
+        const data = JSON.parse(e.target.responseText)
+        console.log(data);
+    }
+});
+request.open('GET', 'https://puzzle.mead.io/puzzle');
+request.send();
