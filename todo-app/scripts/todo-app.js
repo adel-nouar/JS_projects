b7a6b157b9c0accecfe7dec11a3ffc6618fe1f14
Adel NOUAR <adel.nouar@protonmail.com>
2021-07-02 22:22:48 +0200

    To-do App : optimizing the code and styling elements

--- todo-app/scripts/todo-app.js
+++ todo-app/scripts/todo-app.js
@@ -6,34 +6,33 @@ let todos = getSavedTodos();
 const filters = {
     searchText:'',
     hideCompleted: false
-}
+};
 
-renderTodos(todos, filters)
+renderTodos(todos, filters);
 
 document.querySelector('#search-text').addEventListener('input', (e) =>{
-    filters.searchText = e.target.value
-    renderTodos(todos, filters)
+    filters.searchText = e.target.value;
+    renderTodos(todos, filters);
 })
 
-document.querySelector('#new-todo').addEventListener('submit', (e) =>{
+document.querySelector('#new-todo').addEventListener('submit', (e) => {
     const text = e.target.elements.text.value.trim();
     e.preventDefault();
 
     if (text.length > 0) {
+        console.log(text);
         todos.push({
             id:uuidv4(),
             text:text,
             completed: false
-        });
+        })
         saveTodos(todos);
         renderTodos(todos, filters);
         e.target.elements.newTodo.value = '';
     }
-    
-    
 })
 
 document.querySelector('#hide-completed').addEventListener('change', (e) =>{
-    filters.hideCompleted = e.target.checked
-    renderTodos(todos, filters)
+    filters.hideCompleted = e.target.checked;
+    renderTodos(todos, filters);
 })
--- todo-app/scripts/todo-functions.js
+++ todo-app/scripts/todo-functions.js
@@ -1,17 +1,13 @@
 // Functions related to our todo app
 'use strict';
 
-
 const getSavedTodos = () => {
-
     const todosJSON = localStorage.getItem('todos');
-
     try {
-        return todosJSON ? JSON.parse(todosJSON) :[];
+        return todosJSON ? JSON.parse(todosJSON) : [];
     } catch (e) {
         return [];
     }
-    
 }
 
 const saveTodos = (todos) => {
@@ -19,8 +15,7 @@ const saveTodos = (todos) => {
 }
 
 const removeTodo = (id) => {
-    const todoIndex = todos.findIndex((todo) => todo.id === id)
-
+    const todoIndex = todos.findIndex((todo) => todo.id === id);
     if (todoIndex > -1){
         todos.splice(todoIndex, 1);
     }
@@ -28,52 +23,48 @@ const removeTodo = (id) => {
 
 // Toggle the completed value for a given todo
 const toggleTodo = (id) => {
-    const todo = todos.find((todo) => todo.id === id)
-
+    const todo = todos.find((todo) => todo.id === id);
     if (todo){
-        todo.completed = !todo.completed
+        todo.completed = !todo.completed;
     }
 }
 
 const renderTodos = (todos, filters) => {
-
+    const todoEl = document.querySelector('#todos');
     const filteredTodos = todos.filter((todo) => {
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase());
         const hideCompletedMatch = !filters.hideCompleted || !todo.completed;
-        // debugger; 
         return searchTextMatch && hideCompletedMatch;
     })
 
-    const incompleteTodos = filteredTodos.filter((todo) => !todo.completed)
+    const incompleteTodos = filteredTodos.filter((todo) => !todo.completed);
 
-    document.querySelector('#todos').innerHTML = ''
+    todoEl.innerHTML = '';
+    todoEl.appendChild(generateSummaryDOM(incompleteTodos));
 
-    document.querySelector('#todos').appendChild(generateSummaryDOM(incompleteTodos));
-    
-    filteredTodos.forEach((todo) =>{
-        generateTodoDOM(todo);
-        document.querySelector('#todos').appendChild(generateTodoDOM(todo));
-    })
+    if (filteredTodos.length > 0) {
+        filteredTodos.forEach((todo) =>{
+            todoEl.appendChild(generateTodoDOM(todo));
+        })
+    } else {
+        const messageEl = document.createElement('p');
+        messageEl.classList.add('empty-message');
+        messageEl.textContent = 'No to-dos to show';
+        todoEl.appendChild(messageEl);
+    }    
 }
 
 const generateTodoDOM = (todo) => {
-    const todoEl = document.createElement('div');
+    const todoEl = document.createElement('label');
+    const containerEl = document.createElement('div');
     const checkbox = document.createElement('input');
-
     const todoText = document.createElement('span');
-
     const removeButton = document.createElement('button');
-
-    removeButton.addEventListener('click', () =>{
-        removeTodo(todo.id);
-        saveTodos(todos);
-        renderTodos(todos, filters);
-    })
 
     // Setup todo checkbox
     checkbox.setAttribute('type', 'checkbox');
     checkbox.checked = todo.completed;
-    todoEl.appendChild(checkbox);
+    containerEl.appendChild(checkbox);
     checkbox.addEventListener('change', () =>{
         toggleTodo(todo.id);
         saveTodos(todos);
@@ -82,11 +73,24 @@ const generateTodoDOM = (todo) => {
 
     // Setup todo text
     todoText.textContent = todo.text;
-    todoEl.appendChild(todoText);
+    containerEl.appendChild(todoText);
+
+    // Setup container
+    todoEl.classList.add('list-item');
+    containerEl.classList.add('list-item__container');
+    todoEl.appendChild(containerEl)
 
     // Setup the remove button
-    removeButton.textContent = 'x';
+    removeButton.textContent = 'remove';
+    removeButton.classList.add('button', 'button--text')
     todoEl.appendChild(removeButton);
+    removeButton.addEventListener('click', () =>{
+        removeTodo(todo.id);
+        saveTodos(todos);
+        renderTodos(todos, filters);
+    })
+
+
 
     return todoEl;
     
@@ -94,6 +98,8 @@ const generateTodoDOM = (todo) => {
 
 const generateSummaryDOM = (incompleteTodos) => {
     const summary = document.createElement('h2');
-    summary.textContent = `You have ${incompleteTodos.length} todos left`;
+    const plural = incompleteTodos.length === 1 ? '' : 's';
+    summary.classList.add('list-title');
+    summary.textContent = `You have ${incompleteTodos.length} todo${plural} left`;
     return summary;
 }
