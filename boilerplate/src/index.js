debfb821a5342b62a93c4b0113fa16d84434f263
Adel NOUAR <adel.nouar@protonmail.com>
2021-07-11 18:18:12 +0200

    testing spread object

--- boilerplate/src/index.js
+++ boilerplate/src/index.js
@@ -19,3 +19,34 @@ printTeam(team.name, team.coach, ...team.players);
 
 const cities = ['Barcelona', 'Cape Town', 'Bordeaux'];
 const citiesCopy = [...cities] // Copy cities to citiesCopy (each element is copied)
+
+
+// Spread object operator
+let house = {
+    bedrooms: 2,
+    bathrooms: 1.5,
+    yearBuilt: 2017
+}
+let newHouse = {
+    basement: true,
+    ...house,
+    bedrooms: 3
+}
+newHouse.yearBuilt = 2018;
+
+console.log(house);
+console.log(newHouse);
+
+const person = {
+    name: 'Andy',
+    age: 27
+}
+const location = {
+    city: 'London',
+    country: 'UK'
+}
+const overview = {
+    ...person,
+    ...location
+}
+console.log(overview);
--- hangman/webpack.config.js
+++ hangman/webpack.config.js
@@ -13,7 +13,8 @@ module.exports = {
             use:{
                 loader: 'babel-loader',
                 options: {
-                    presets: ['@babel/preset-env']
+                    presets: ['@babel/preset-env'],
+                    plugins: ['transform-object-rest-spread']
                 }
             }
         }]
